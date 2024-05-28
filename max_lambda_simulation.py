d9d5f8af45246cde9ce3419662877be669e5d907
Marcel_Garczyk <marcel.garczyk@pit.lukasiewicz.gov.pl>
2024-05-28 19:57:36 +0200

    Sleep treshold into config.

--- max_lambda_simulation.py
+++ max_lambda_simulation.py
@@ -136,7 +136,7 @@ def init_generator(simulation_counter : int):
 
 def init_simulation(count : int, simulation_counter : int):
     print(BETA_MIN, BETA_MAX)
-    beta_list = np.arange(BETA_MAX, BETA_MIN, BETA_STEP)  # Wektory tetstowe  [0.1, 0.8, 0.9] [0.010, 0.011, 0.012]
+    beta_list = np.arange(BETA_MAX, BETA_MIN+BETA_STEP, BETA_STEP)  # Wektory tetstowe  [0.1, 0.8, 0.9] [0.010, 0.011, 0.012]
     beta_list = np.flip(beta_list)
     print(beta_list)
     os.makedirs(f'wyniki_lambda_max/wyniki_{count}/symulacja_{simulation_counter}/hist/tau')
